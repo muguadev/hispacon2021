4f87b466d6ef38d24de00955d3cf70dc492fa70e
jcarlos.teso <jcarlos.teso@gmail.com>
2021-10-27 09:51:44 +0200

    Change schedule data to UTF8 encoding

--- data/programa.js
+++ data/programa.js
@@ -1,68 +1,68 @@
-export const SCHEDULE = `Evento	Sala	Fecha	Duración	Tipo	Sede	Título	Link
-E001	Ignotus	19/11/2021 10:00	30	Evento oficial		Inauguración	
-E002	Visiones	19/11/2021 10:30	30	Presentación de novedades		Presentación de: Arima	
+export const SCHEDULE = `Evento	Sala	Fecha	DuraciÃ³n	Tipo	Sede	TÃ­tulo	Link
+E001	Ignotus	19/11/2021 10:00	30	Evento oficial		InauguraciÃ³n	
+E002	Visiones	19/11/2021 10:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Arima	
 E003	Microsedes	19/11/2021 10:00	60	Mesa redonda	80 Mundos	Are we the baddies? Nadie es el villano de su propia historia	
-E004	Visiones	20/11/2021 10:00	30	Presentación de novedades		Presentación de Aloha: «Hermana X. Nunsploitation & pulp en la edición actual»	
-E005	Visiones	20/11/2021 10:30	30	Presentación de novedades		Presentación de: Aristas Martínez	
+E004	Visiones	20/11/2021 10:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de Aloha: Â«Hermana X. Nunsploitation & pulp en la ediciÃ³n actualÂ»	
+E005	Visiones	20/11/2021 10:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Aristas MartÃ­nez	
 E006	Ignotus	20/11/2021 10:00	60	Mesa redonda		Mesa autores chines	
-E007	Visiones	21/11/2021 10:00	30	Presentación de novedades		Presentación de Niña Loba: «Las aventuras de Lutven Karena», de Luis Zurriaga	
-E008	Visiones	21/11/2021 10:30	30	Presentación de novedades		Presentación de: Premium	
-E009	Ignotus	21/11/2021 10:30	30	Presentación		Tolkien <i>mainstream</i>	
-E010	Visiones	19/11/2021 11:00	30	Presentación de novedades		Presentación de Roomie: «Nana de Sombras»	
-E011	Visiones	19/11/2021 11:30	30	Presentación de novedades		Presentación de: Insomnia	
-E012	Ignotus	19/11/2021 11:00	60	Mesa redonda		Mortífagos en Invernalia: La traducción de neologismos en la fantasía, el terror y la ciencia ficción	
-E013	Visiones	20/11/2021 11:00	30	Presentación de novedades		Soy lo que me persigue: El terror como ficción del trauma	
-E014	Visiones	20/11/2021 11:30	30	Presentación de novedades		Presentación de: La biblioteca de Carfax	
-E015	Ignotus	20/11/2021 11:00	60	Mesa redonda		Misma historia, otros ojos: La religión y el mito en los videojuegos	
-E016	Visiones	21/11/2021 11:00	30	Presentación de novedades		Presentación de: Literup	
-E017	Visiones	21/11/2021 11:30	30	Presentación de novedades		Presentación de: Con pluma y píxel	
+E007	Visiones	21/11/2021 10:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de NiÃ±a Loba: Â«Las aventuras de Lutven KarenaÂ», de Luis Zurriaga	
+E008	Visiones	21/11/2021 10:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Premium	
+E009	Ignotus	21/11/2021 10:30	30	PresentaciÃ³n		Tolkien <i>mainstream</i>	
+E010	Visiones	19/11/2021 11:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de Roomie: Â«Nana de SombrasÂ»	
+E011	Visiones	19/11/2021 11:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Insomnia	
+E012	Ignotus	19/11/2021 11:00	60	Mesa redonda		MortÃ­fagos en Invernalia: La traducciÃ³n de neologismos en la fantasÃ­a, el terror y la ciencia ficciÃ³n	
+E013	Visiones	20/11/2021 11:00	30	PresentaciÃ³n de novedades		Soy lo que me persigue: El terror como ficciÃ³n del trauma	
+E014	Visiones	20/11/2021 11:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: La biblioteca de Carfax	
+E015	Ignotus	20/11/2021 11:00	60	Mesa redonda		Misma historia, otros ojos: La religiÃ³n y el mito en los videojuegos	
+E016	Visiones	21/11/2021 11:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Literup	
+E017	Visiones	21/11/2021 11:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Con pluma y pÃ­xel	
 E018	Ignotus	21/11/2021 11:00	60	Mesa redonda		Porque yo lo valgo: Autopublicar con calidad profesional	
-E019	Visiones	19/11/2021 12:00	30	Presentación de novedades		Presentación de: Minotauro	
-E020	Visiones	19/11/2021 12:30	30	Presentación de novedades		Crononauta en 2022: Novedades a raudales	
+E019	Visiones	19/11/2021 12:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Minotauro	
+E020	Visiones	19/11/2021 12:30	30	PresentaciÃ³n de novedades		Crononauta en 2022: Novedades a raudales	
 E021	Microsedes	19/11/2021 12:00	60	Mesa redonda	80 Mundos	La vida secreta de las casas encantadas	
-E022	Visiones	20/11/2021 12:00	30	Presentación de novedades		Presentación de: Cerbero	
-E023	Visiones	20/11/2021 12:30	30	Presentación de novedades		Presentación de Pez de Plata: «Hambrientos y cobardes»	
+E022	Visiones	20/11/2021 12:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Cerbero	
+E023	Visiones	20/11/2021 12:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de Pez de Plata: Â«Hambrientos y cobardesÂ»	
 E024	Ignotus	20/11/2021 12:00	60	Mesa redonda		Worldbuilding	
-E025	Visiones	21/11/2021 12:00	30	Presentación de novedades		Presentación de: Duermevela	
-E026	Visiones	21/11/2021 12:30	30	Presentación de novedades		Presentación de: Dorna	
-E027	Ignotus	21/11/2021 12:00	60	Evento oficial		Presentación de «Fabricantes de sueños 2016-2017»	
-E028	Ignotus	19/11/2021 13:00	60	Mesa redonda		Humor y literatura fantástica: Sí, siempre	
-E029	Visiones	19/11/2021 13:00	60	Mesa redonda		El infinito en un verso: Poesía de ciencia ficción y fantasía	
-E030	Microsedes	20/11/2021 13:00	60	Mesa redonda	Akira	De «El día de la Bestia» a «El laberinto del fauno»: medio siglo y 9 Goyas haciendo magia	
-E031	Visiones	20/11/2021 13:00	60	Mesa redonda		Editando librojuegos: Cómo, qué y por qué	
-E032	Ignotus	20/11/2021 13:00	60	Mesa redonda		Esto es lo que hacía Ibai, ¿verdad?: Cómo divulgar literatura en Twitch	
+E025	Visiones	21/11/2021 12:00	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Duermevela	
+E026	Visiones	21/11/2021 12:30	30	PresentaciÃ³n de novedades		PresentaciÃ³n de: Dorna	
+E027	Ignotus	21/11/2021 12:00	60	Evento oficial		PresentaciÃ³n de Â«Fabricantes de sueÃ±os 2016-2017Â»	
+E028	Ignotus	19/11/2021 13:00	60	Mesa redonda		Humor y literatura fantÃ¡stica: SÃ­, siempre	
+E029	Visiones	19/11/2021 13:00	60	Mesa redonda		El infinito en un verso: PoesÃ­a de ciencia ficciÃ³n y fantasÃ­a	
+E030	Microsedes	20/11/2021 13:00	60	Mesa redonda	Akira	De Â«El dÃ­a de la BestiaÂ» a Â«El laberinto del faunoÂ»: medio siglo y 9 Goyas haciendo magia	
+E031	Visiones	20/11/2021 13:00	60	Mesa redonda		Editando librojuegos: CÃ³mo, quÃ© y por quÃ©	
+E032	Ignotus	20/11/2021 13:00	60	Mesa redonda		Esto es lo que hacÃ­a Ibai, Â¿verdad?: CÃ³mo divulgar literatura en Twitch	
 E033	Ignotus	21/11/2021 13:00	60	Mesa redonda		Joven promesa a los 40: La experiencia de ser autores noveles cuando ya se peinan canas	
-E034	Ignotus	19/11/2021 15:00	60	Presentación		No te limites al grifo: aves mitológicas menos conocidas para dar variedad a tus escritos	
-E035	Ignotus	20/11/2021 15:00	60	Mesa redonda		<i>Noir</i> fantástico: una combinación fabulosa	
+E034	Ignotus	19/11/2021 15:00	60	PresentaciÃ³n		No te limites al grifo: aves mitolÃ³gicas menos conocidas para dar variedad a tus escritos	
+E035	Ignotus	20/11/2021 15:00	60	Mesa redonda		<i>Noir</i> fantÃ¡stico: una combinaciÃ³n fabulosa	
 E036	Visiones	20/11/2021 15:00	60	Mesa redonda		Windumanoth podcast en directo	
-E037	Ignotus	21/11/2021 15:00	60	Presentación		La ficción como reflejo de la historia y la economía	
-E038	Visiones	21/11/2021 15:00	60	Mesa redonda		De cómo gestionar un <i>crowdfunding</i> y no perder a nadie por el camino	
+E037	Ignotus	21/11/2021 15:00	60	PresentaciÃ³n		La ficciÃ³n como reflejo de la historia y la economÃ­a	
+E038	Visiones	21/11/2021 15:00	60	Mesa redonda		De cÃ³mo gestionar un <i>crowdfunding</i> y no perder a nadie por el camino	
 E039	Ignotus	19/11/2021 16:00	60	Mesa redonda		Cordilleras de tinta: Rol y mapas	
 E040	Visiones	19/11/2021 16:00	60	Mesa redonda		Ecofeminismo: Sin igualdad no salvaremos la Tierra	
-E041	Microsedes	19/11/2021 16:00	60	Mesa redonda	Pantera Rossa	El futuro del futuro: Imaginando la ciencia ficción de la sociedad postcapitalista	
+E041	Microsedes	19/11/2021 16:00	60	Mesa redonda	Pantera Rossa	El futuro del futuro: Imaginando la ciencia ficciÃ³n de la sociedad postcapitalista	
 E042	Ignotus	20/11/2021 16:00	60	Mesa redonda		Leti con la UTAD	
-E043	Microsedes	20/11/2021 16:00	60	Entrevista	Domus	Conversación con… Conchi Regueiro	
-E044	Visiones	20/11/2021 16:00	60	Mesa redonda		Radioficción y arqueología literaria	
-E045	Ignotus	21/11/2021 16:00	60	Mesa redonda		Rol de hemen, enllà y acolá: Rol en las lenguas minorizadas	
-E046	Microsedes	21/11/2021 16:00	60	Mesa redonda	Pantera Rossa	Gente que hace cosas: Creación de espacios culturales propios	
-E047	Ignotus	19/11/2021 17:00	60	Evento oficial		Presentación del Premio Domingo Santos 2021 de novela	
-E048	Microsedes	20/11/2021 17:00	60	Mesa redonda	Domus	Crónicas (cientíticas) marcianas	
+E043	Microsedes	20/11/2021 16:00	60	Entrevista	Domus	ConversaciÃ³n conâ€¦ Conchi Regueiro	
+E044	Visiones	20/11/2021 16:00	60	Mesa redonda		RadioficciÃ³n y arqueologÃ­a literaria	
+E045	Ignotus	21/11/2021 16:00	60	Mesa redonda		Rol de hemen, enllÃ  y acolÃ¡: Rol en las lenguas minorizadas	
+E046	Microsedes	21/11/2021 16:00	60	Mesa redonda	Pantera Rossa	Gente que hace cosas: CreaciÃ³n de espacios culturales propios	
+E047	Ignotus	19/11/2021 17:00	60	Evento oficial		PresentaciÃ³n del Premio Domingo Santos 2021 de novela	
+E048	Microsedes	20/11/2021 17:00	60	Mesa redonda	Domus	CrÃ³nicas (cientÃ­ticas) marcianas	
 E049	Ignotus	20/11/2021 17:00	60	Mesa redonda		Rol en el aula	
-E050	Microsedes	20/11/2021 17:00	60	Mesa redonda	Akira	Cuéntala otra vez: El <i>retelling</i> como técnica narrativa	
+E050	Microsedes	20/11/2021 17:00	60	Mesa redonda	Akira	CuÃ©ntala otra vez: El <i>retelling</i> como tÃ©cnica narrativa	
 E051	Ignotus	21/11/2021 17:00	60	Mesa redonda		Convenciones postpandemia	
-E052	Visiones	21/11/2021 17:00	60	Presentación		La arquitectura de la sangre: El vampiro en representaciones multiplataforma	
+E052	Visiones	21/11/2021 17:00	60	PresentaciÃ³n		La arquitectura de la sangre: El vampiro en representaciones multiplataforma	
 E053	Ignotus	19/11/2021 18:00	60	Mesa redonda		El vampiro en la literatura: mitos, evoluciones y otras idas de olla	
-E054	Visiones	19/11/2021 18:00	60	Mesa redonda		Héroes, villanos y «lo de en medio». Mesa redonda sobre personajes.	
-E055	Ignotus	20/11/2021 18:00	60	Mesa redonda		Literatura latinoamericana: Estrategias de resistencia desde los géneros no realistas	
-E056	Visiones	20/11/2021 18:00	60	Mesa redonda		<i>Serial killers:</i> Los asesinos en serie y la ficción (cine, literatura, cómic, videojuego)	
-E057	Microsedes	21/11/2021 18:00	60	Mesa redonda	Insòlita	Literatura de género en catalán	
+E054	Visiones	19/11/2021 18:00	60	Mesa redonda		HÃ©roes, villanos y Â«lo de en medioÂ». Mesa redonda sobre personajes.	
+E055	Ignotus	20/11/2021 18:00	60	Mesa redonda		Literatura latinoamericana: Estrategias de resistencia desde los gÃ©neros no realistas	
+E056	Visiones	20/11/2021 18:00	60	Mesa redonda		<i>Serial killers:</i> Los asesinos en serie y la ficciÃ³n (cine, literatura, cÃ³mic, videojuego)	
+E057	Microsedes	21/11/2021 18:00	60	Mesa redonda	InsÃ²lita	Literatura de gÃ©nero en catalÃ¡n	
 E058	Visiones	21/11/2021 18:00	60	Mesa redonda		La Tercera Nave: Bibliotecas para encontrarlos a todos	
-E059	Ignotus	21/11/2021 18:00	60	Evento oficial		Presentación de la antología «Visiones 2021»	
-E060	Ignotus	19/11/2021 19:00	60	Entrevista		Conversación con… Jo Walton	
-E061	Microsedes	19/11/2021 19:00	60	Mesa redonda	Gigamesh	Mesa sobre escribir Ciencia Ficción	
-E062	Microsedes	20/11/2021 19:00	60	Mesa redonda	Gigamesh	Mesa de cómic	
-E063	Ignotus	20/11/2021 19:00	60	Entrevista		Conversación con… Silvia Moreno-García	
-E064	Visiones	20/11/2021 19:00	60	Entrevista		Conversación con… Derek Kunsken	
-E065	Ignotus	21/11/2021 19:00	60	Entrevista		Conversación con… Seanan McGuire	
-E066	Visiones	21/11/2021 19:00	60	Mesa redonda		La Academia contraataca: Reflexión y resistencia en torno a la narrativa de género latinoamericana	
-E067	Ignotus	20/11/2021 21:00	60	Evento oficial		Presentación de los Premios Ignotus 2021`
+E059	Ignotus	21/11/2021 18:00	60	Evento oficial		PresentaciÃ³n de la antologÃ­a Â«Visiones 2021Â»	
+E060	Ignotus	19/11/2021 19:00	60	Entrevista		ConversaciÃ³n conâ€¦ Jo Walton	
+E061	Microsedes	19/11/2021 19:00	60	Mesa redonda	Gigamesh	Mesa sobre escribir Ciencia FicciÃ³n	
+E062	Microsedes	20/11/2021 19:00	60	Mesa redonda	Gigamesh	Mesa de cÃ³mic	
+E063	Ignotus	20/11/2021 19:00	60	Entrevista		ConversaciÃ³n conâ€¦ Silvia Moreno-GarcÃ­a	
+E064	Visiones	20/11/2021 19:00	60	Entrevista		ConversaciÃ³n conâ€¦ Derek Kunsken	
+E065	Ignotus	21/11/2021 19:00	60	Entrevista		ConversaciÃ³n conâ€¦ Seanan McGuire	
+E066	Visiones	21/11/2021 19:00	60	Mesa redonda		La Academia contraataca: ReflexiÃ³n y resistencia en torno a la narrativa de gÃ©nero latinoamericana	
+E067	Ignotus	20/11/2021 21:00	60	Evento oficial		PresentaciÃ³n de los Premios Ignotus 2021`
